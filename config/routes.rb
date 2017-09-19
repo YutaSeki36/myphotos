30e890778e2f36a07ef085d1bdec223b29876606
YutaSeki36 <sekiyuta0306@gmail.com>
2017-09-20 00:30:07 +0900

    adminユーザ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+  get 'homes/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
